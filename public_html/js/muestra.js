1f18903d246fc957ff6703c13f0c3d8b933bc718
Ivan-hdz <honter1997@gmail.com>
2015-05-05 13:13:40 -0500

    Le puse mas usabilidad

--- public_html/js/muestra.js
+++ public_html/js/muestra.js
@@ -7,16 +7,14 @@ $(document).ready(function(){
     $('#muestrame').click(function(){
         $('nav').toggleClass('mostrar'); 
     });
+    $('nav').mouseleave(function(){
+        $('nav').toggleClass('mostrar');
+    });
     $('.mostrarLink').click(function(){
         $('.mostrarSeccion').slideToggle(1000);
     });
-});
 
+});
 function mostrar(menu){
-    if(menu.style.display === "none"){
-        menu.style.display = "inline";
-    }else{
-        menu.style.display = "none";
-    }
-    
+    $(menu).slideToggle(800);
 }
